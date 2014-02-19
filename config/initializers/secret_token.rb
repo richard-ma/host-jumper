a1f5911178db1659e24df02caa32fe3d5c174697
Richard Ma <richard.ma.19850509@gmail.com>
2014-02-19 20:34:52 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HostJumper::Application.config.secret_key_base = 'ad84b901c5813e43c8203132d2cdb1f6833f5d611f4f62d161ce24c8ad5ae8d7ec54bac8c6d04d20d7ffa4a6f73523c7fc286b8dfc0f496dc70a63fc0317ebfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HostJumper::Application.config.session_store :cookie_store, key: '_host-jumper_session'
