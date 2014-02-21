b558e13fb55395cb6e8b299296a691b3f0843278
Richard Ma <richard.ma.19850509@gmail.com>
2014-02-21 13:11:49 +0800

    add host path function test

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,6 +2,6 @@ class Host < ActiveRecord::Base
 
   # format host access url
   def path
-    @port == nil ? "http://#{@ip}#{@suffix}" : "http://#{@ip}:#{@port}#{@suffix}"
+    port == nil ? "http://#{ip}#{suffix}" : "http://#{ip}:#{port}#{suffix}"
   end
 end
--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class HostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Should return full URL" do
+    host = hosts(:one)
+    assert_equal host.path, "http://#{host.ip}:#{host.port}#{host.suffix}", "host.path: #{host.path}"
+  end
+
+  test "Should return URL without port" do
+    host = hosts(:without_port)
+    assert_equal host.path, "http://#{host.ip}#{host.suffix}", "host.path: #{host.path}"
+  end
 end
