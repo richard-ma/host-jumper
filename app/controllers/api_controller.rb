7d8e383b0b4c7454a6a22871df531070aafaf523
Richard Ma <richard.ma.19850509@gmail.com>
2014-07-31 09:46:07 +0800

    add heartbeat min interval limit

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,11 @@
 class ApiController < ApplicationController
-  before_action :set_host, only: [:heartbeat]
+  before_action :set_host, only: [:heartbeat, :is_attack?]
 
   # GET /api/heartbeat/:id/:token
   # GET /api/heartbeat/:id/:token.json
   def heartbeat
+    not_found if be_attacking?
+      
     @host.ip = request.remote_ip
     @host.updated_at = Time.now.to_time
 
@@ -11,8 +13,15 @@ class ApiController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_host
-      @host = Host.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_host
+    @host = Host.find(params[:id])
+  end
+  
+  def be_attacking?
+    ret = false
+
+    # heartbeat too often
+    ret = true if (Time.now - @host.updated_at).to_i < Figaro.env.min_heartbeat_interval.to_i
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Show 404 page
+  # http://stackoverflow.com/questions/2385799/how-to-redirect-to-a-404-in-rails
+  def not_found 
+    raise ActionController::RoutingError.new('now found')
+  end
 end
